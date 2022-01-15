7b3a6135a658026f348d06292b345c443f19906a
laherasif <47274948+laherasif@users.noreply.github.com>
2022-01-15 22:54:15 +0500

    working on checkout page

--- frontend/src/component/layout/Checkout/Checkout.jsx
+++ frontend/src/component/layout/Checkout/Checkout.jsx
@@ -1,9 +1,42 @@
-import  './checkout.css'
+import './checkout.css'
 
 export default function Checkout() {
     return (
         <>
-           checkout  
+            <section className="checkoutWrapper">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <h4 className="check_heading">Checkout Shopping</h4>
+                            <hr/>
+                            <div className="shipping_address">
+                                <form>
+                                    <div className="mb-2">
+                                        <label className="form-label">Your name</label>
+                                        <input type="email" className="form-control" />
+                                    </div>
+                                    <div className="mb-2">
+                                        <label className="form-label">Email Address </label>
+                                        <input type="email" className="form-control" />
+                                    </div>
+                                    <div className="mb-2">
+                                        <label className="form-label">Shipping Address </label>
+                                        <input type="email" className="form-control" />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label className="form-label">Mobile number</label>
+                                        <input type="email" className="form-control" />
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            right
+                        </div>
+                    </div>
+                </div>
+
+            </section>
         </>
     )
 }
