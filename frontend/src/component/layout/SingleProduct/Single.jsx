527c5f8240b5b3c9db1bec1969448eeac184e0cf
laherasif <47274948+laherasif@users.noreply.github.com>
2022-01-15 21:33:05 +0500

    left side image upload

--- frontend/src/component/layout/SingleProduct/Single.jsx
+++ frontend/src/component/layout/SingleProduct/Single.jsx
@@ -3,19 +3,23 @@ import './single.css'
 export default function Single() {
     return (
         <>
-            <section>
+            <section className="singleWrapper">
                 <div className="container-fluid">
                     <div className="add_img">
                         <img src="/images/shopcar.png" alt="add_image" className="amazon_add" />
                     </div>
                     <nav className="nav_breadcrumb">
                         <ol className="breadcrumb">
-                            <li className="breadcrumb-item"><a href="#">Home</a></li>
-                            <li className="breadcrumb-item active" aria-current="page">Library</li>
+                            <li className="breadcrumb-item">Home</li>
+                            <li className="breadcrumb-item active" ariaCurrent="page">Library</li>
                         </ol>
                     </nav>
                     <div className="row">
-                        <div className="col-md-5"></div>
+                        <div className="col-md-5">
+                            <div className="single_image">
+                               <img src="/images/chair-1.jpg" alt="single_image" className="single_img"/>
+                            </div>
+                        </div>
                         <div className="col-md-7">right</div>
                     </div>
                 </div>
