fb00ff996fad24f683183e3a126606b23d9d8be8
laherasif <47274948+laherasif@users.noreply.github.com>
2022-01-15 21:46:50 +0500

    width update

--- frontend/src/component/layout/SingleProduct/Single.jsx
+++ frontend/src/component/layout/SingleProduct/Single.jsx
@@ -15,14 +15,23 @@ export default function Single() {
                         </ol>
                     </nav>
                     <div className="row">
-                        <div className="col-md-5">
+                        <div className="col-md-4">
                             <div className="single_image">
                                 <img src="/images/chair-1.jpg" alt="single_image" className="single_img" />
                             </div>
                         </div>
-                        <div className="col-md-7">
+                        <div className="col-md-8">
                              <div className="centerWrapper">
-                                <div className="single_content">center</div>
+                                <div className="single_content">
+                                   <h4 className="single_product_detail">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda ea laboriosam, voluptate quia sunt numquam explicabo totam quasi incidunt quos dicta officiis repudiandae error temporibus, quae voluptates excepturi consequatur impedit.</h4>
+                                    <p className="price">$ 14.40</p>
+                                    <ul className="product_info">
+                                    <li className="listInfo">
+                                      <h3>Product Dimentions</h3>
+                                      <p>24.4"D x 26.4</p>
+                                    </li>
+                                    </ul>
+                                </div>
                                 <div className="addCart_content"> right</div>
                              </div>
                         </div>
