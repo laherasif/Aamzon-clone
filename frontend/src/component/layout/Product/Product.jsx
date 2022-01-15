e55b789c096d636d2b280445a956934ffaf92694
laherasif <47274948+laherasif@users.noreply.github.com>
2022-01-15 12:52:17 +0500

    working in product page
    Make two section sidebar and product section

--- frontend/src/component/layout/Product/Product.jsx
+++ frontend/src/component/layout/Product/Product.jsx
@@ -8,8 +8,45 @@ export default function Products() {
             <div className="ProductWrapper">
                 <div className="container-fluid">
                     <div className="row">
-                        <div className="col-md-3 col-lg-3">left</div>
-                        <div className="col-md-9 col-lg-9">right</div>
+                        <div className="col-md-3 col-lg-2">
+                            <div className="sidebar_filter">
+                                <div className="filter">
+                                    Filters
+                                </div>
+                                <hr />
+                                <div className="filter_list">
+                                    <ul className="Lists">
+                                        <li className="listItems">
+                                            Prime & Delivery
+                                </li>
+                                        <li className="listItems">
+                                            Prime & Delivery
+                                </li>
+                                        <li className="listItems">
+                                            Prime & Delivery
+                                </li>
+                                        <li className="listItems">
+                                            Prime & Delivery
+                                </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-9 col-lg-10">
+                            <div className="products">
+                                <div className="price">
+                                    Price and other details may vary base on product size and color
+                                </div>
+                                <div className="products_list">
+                                <div className="row">
+                                <div className="col-md-3">first</div>
+                                <div className="col-md-3">first</div>
+                                <div className="col-md-3">first</div>
+                                <div className="col-md-3">first</div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
                 </div>
